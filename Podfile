3638cbcbd75c3aca74d1b758b5a83ef79bea2db0
wenbin chen <wenbin.chen@autodesk.com>
2019-02-25 10:36:09 -0500

    init

---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'ShareCost' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'RealmSwift'
+  pod "PromiseKit", "~> 6.0"
+  pod 'Alamofire', '~> 4.7'
+  pod 'PromiseKit/Alamofire', '~> 6.0'
+
+  # Pods for ShareCost
+  
+  target 'ShareCostTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ShareCostUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ ShareCost/NetworkFiles/NetworkService.swift
@@ -0,0 +1,24 @@
+//
+//  NetworkService.swift
+//  ShareCost
+//
+//  Created by Wenbin Chen on 2/23/19.
+//  Copyright © 2019 share-cost-project. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import PromiseKit
+
+func getLoginFromString(completionBlock: @escaping (String?) -> Void) {
+    Alamofire.request("https://share-cost.herokuapp.com/test/get").responseJSON { response in
+        if let JSON = response.result.value {
+            print("JSON: \(JSON)")
+            if let resultDict = JSON as? NSDictionary {
+                if let resultString = resultDict["test"] as? String {
+                    completionBlock(resultString)
+                }
+            }
+        }
+    }
+}
